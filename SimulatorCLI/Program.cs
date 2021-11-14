8869fadaf5d28495238db674cc440d85e0adb9a7
Marinela <maryonella02@gmail.com>
2021-11-14 14:31:58 +0200

    Refactor the code

--- SimulatorCLI/Program.cs
+++ SimulatorCLI/Program.cs
@@ -7,12 +7,12 @@ namespace SimulatorCLI
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(100, 40);
+            SetWindowProperties();
             ShowBanner(30);
 
             while (true)
             {
-                Console.Write("\n\n" + Environment.CommandLine + "> ");
+                ShowCurrentDirectoryLine();
                 var a = Console.ReadLine();
                 switch (a)
                 {
@@ -35,17 +35,27 @@ namespace SimulatorCLI
                 }
             }
         }
-        static void ShowBanner(int interval)
+        static void SetWindowProperties()
         {
             Console.Title = "Lab 1 CLI simulator";
-            TypeLine("\t \t Welcome to CLI simulator, " + Environment.UserName + "!", interval);
+            Console.SetWindowSize(100, 40);
+        }
+        static void ShowBanner(int interval)
+        {
+            TypeLine("\t \t Welcome to the CLI simulator, " + Environment.UserName + "!", interval);
             TypeLine("\n\t Your current OS version is: " + Environment.OSVersion, interval);
             TypeLine("\n\t And it's running on machine: " + Environment.MachineName, interval);
             TypeLine("\n\nType 'help' + ENTER -- for available commands.", interval);
         }
+        static void ShowCurrentDirectoryLine()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("\n\n" + Environment.CommandLine + "> ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
         static void ShowHelpResult()
         {
-            Console.WriteLine("\nType [command] + ENTER");
+            Console.WriteLine("\nType [command] + ENTER \n");
             Console.WriteLine("'clear' -- clears the CLI simulator window");
             Console.WriteLine("'exit' -- closes the CLI simulator window");
             Console.WriteLine("'help' -- returns the list of available commands");
